16937d909f41ae66477ed65e0ae6e66350734914
Talique <ropars.k@gmail.com>
2023-09-20 08:16:40 +0200

    add api to get adress insteat of coordonate

--- conponent/ContactForm.js
+++ conponent/ContactForm.js
@@ -2,6 +2,7 @@ import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 import {useState} from 'react';
 import {Marker} from 'react-native-maps';
+import axios from 'axios';
 
 import React from 'react';
 import MapView from 'react-native-maps';
@@ -10,27 +11,46 @@ import SelectDropdown from "react-native-select-dropdown";
 
 const AlerteType = ["Voirie", "stationnement", "travaux"];
 
-export default function ContactForm() {
-    const[mapRegion, setMapRegion] = useState({
-        latitude: 43.475576,
-        longitude: 0.912082,
-        latitudeDelta: 0.0200,
-        longitudeDelta: 0.0200,
-    })
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        defaultValues: {
-            firstName: "",
-            lastName: "",
-        },
+const baseUrl = 'https://api.geoapify.com/v1/geocode/reverse';
+const ApiKeyGeoApiFy = "27899a6a835243f6bcff91ddf2e1dd47";
+
+    
+
+    
+    export default function ContactForm() {
+        const[pinedAdresse, setPinedAdresse] = useState(null);
+        const[mapRegion, setMapRegion] = useState({
+            latitude: 43.475576,
+            longitude: 0.912082,
+            latitudeDelta: 0.0200,
+            longitudeDelta: 0.0200,
+        });
+        
+        
+        const {
+            control,
+            handleSubmit,
+            formState: { errors },
+        } = useForm({
     });
+
+
    
-    const onSubmit = (data) => {
-        console.log(data);
-    };
+    const onSubmit = (data) =>
+        axios.get(`${baseUrl}?lat=${mapRegion.latitude}&lon=${mapRegion.longitude}&format=json&apiKey=${ApiKeyGeoApiFy}`)
+        .then(function (response) {
+            setPinedAdresse(response.data.results[0].formatted);
+            console.log(data);
+            console.log(pinedAdresse)
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });  
+    
+    
     
     return (
         <View>
@@ -42,11 +62,11 @@ export default function ContactForm() {
                 <SelectDropdown data={AlerteType} onSelect={onChange} value={value} defaultButtonText="Choississez une option" />
                 )}
                 name="AlertType"
-            />
+                />
             <Controller
                 control={control}
                 rules={{
-                required: true,
+                    required: true,
                 }}
                 render={({ field: { onChange, onBlur, value } }) => (
                 <TextInput
@@ -117,7 +137,11 @@ export default function ContactForm() {
             name="map"
         />
 
-            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+            <Button title="Submit" onPress={handleSubmit(onSubmit)}/>
+            
+            <View>
+                <Text>{pinedAdresse}</Text>
+            </View>
         </View>
     );
 }
