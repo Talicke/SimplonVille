e9e7d450d7efc900bcaa00fed76a630379fed09d
Talique <ropars.k@gmail.com>
2023-09-21 17:25:01 +0200

    get localiton on start app, and send mail (oops)

--- app/conponent/ContactForm.js
+++ app/conponent/ContactForm.js
@@ -5,6 +5,8 @@ import {useState, useEffect} from 'react';
 import {Marker} from 'react-native-maps';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import * as Location from 'expo-location'
+import emailjs from '@emailjs/browser';
+
 
 import axios from 'axios';
 
@@ -23,19 +25,72 @@ const ApiKeyGeoApiFy = "27899a6a835243f6bcff91ddf2e1dd47";
 
 export default function ContactForm() {
     const[mapRegion, setMapRegion] = useState({
-        latitude:43.469955405214364, 
-        longitude: 0.9255059046242529,
+        latitude:48.855579517566845, 
+        longitude: 2.328495571923907,
+        latitudeDelta: 0.0200,
+        longitudeDelta: 0.0200,
     });
     const[pinnedLoc, setPinnedLoc] = useState({
-        latitude:43.469955405214364, 
-        longitude: 0.9255059046242529,
+        latitude:48.855579517566845, 
+        longitude: 2.328495571923907,
         latitudeDelta: 0.0200,
         longitudeDelta: 0.0200,
     })
 
+    
+    
     const[pinedAdresse, setPinedAdresse] = useState(null);
     
+    const [locFind, setLocFind] = useState(false); 
     
+    const sendEmail = (data) => {
+    
+        emailjs.send('service_2v0laq8', 'template_i5mh4il', 
+        {
+            from_name : data.email,
+            to_name : "WebAppli Licornoville",
+            description_event: data.Description,
+            adresse_event: pinedAdresse,
+            nom: data.nom,
+            prenom: data.prenom
+
+        }
+        , '8Yo7fTo5ZtEA_w19F')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+    
+    const getLoc = async () => {
+        
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            console.log('Permission to access location was denied');
+            return;
+        }
+
+        let location = (await Location.getCurrentPositionAsync({}));
+
+        setMapRegion({
+            latitude:location.coords.latitude, 
+            longitude:location.coords.longitude,
+            latitudeDelta: 0.0200,
+            longitudeDelta: 0.0200,
+        });
+        setPinnedLoc({
+            latitude:location.coords.latitude, 
+            longitude:location.coords.longitude,
+            latitudeDelta: 0.0200,
+            longitudeDelta: 0.0200,
+            });
+        if(locFind == false){
+            setLocFind(true);
+        }
+      };
+
+
     const {
         control,
         handleSubmit,
@@ -53,79 +108,85 @@ export default function ContactForm() {
         })
     }
 
-    const onSubmit = () => {
-      
+    const onSubmit = (data) => {
+        console.log(data)
+        sendEmail(data);
     }
     
-    
-    
+    useEffect(() => {
+        getLoc()
+        if(locFind){
+            getAdress();
+        }
+    },[locFind]) 
     
     return (
         <ScrollView>
-            <Text style={styles.titre}>Signalez !</Text>
-          
+            <View style={styles.formContainer}>
+                <Text style={styles.titre}>Signalez !</Text>
             
-            <View>
-                <Text>Votre prenom :</Text>
+                
+                <View>
+                    <Text>Votre prenom :</Text>
+                        <Controller
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Jean"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                style = {styles.input}
+                            />
+                            )}
+                            name="prenom"
+                        />
+                        {errors.AlerteType && <Text>This is required.</Text>}
+                </View>
+                
+                <View>
+                    <Text>Votre Nom : </Text>
                     <Controller
+                        
                         control={control}
                         rules={{
-                            required: true,
+                        maxLength: 100,
                         }}
                         render={({ field: { onChange, onBlur, value } }) => (
                         <TextInput
-                            placeholder="Jean"
+                            placeholder="Nom"
                             onBlur={onBlur}
                             onChangeText={onChange}
                             value={value}
                             style = {styles.input}
                         />
                         )}
-                        name="Prenom"
+                        name="nom"
                     />
-                    {errors.AlerteType && <Text>This is required.</Text>}
-            </View>
-            
-            <View>
-                <Text>Votre Nom : </Text>
-                <Controller
-                    
-                    control={control}
-                    rules={{
-                    maxLength: 100,
-                    }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                        placeholder="Nom"
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        value={value}
-                        style = {styles.input}
-                    />
-                    )}
-                    name="Nom"
-                />
-                {errors.Nom && <Text>This is required.</Text>}
-            </View>
+                    {errors.Nom && <Text>This is required.</Text>}
+                </View>
 
-            <View>
-                <Text>Votre E-mail : </Text>
-                <Controller
-                    
-                    control={control}
-                    rules={{
-                    maxLength: 100,
-                    }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                        placeholder="mail@email.com"
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        value={value}
-                        style = {styles.input}
-                    />
-                    )}
-                    name="email"
+                <View>
+                    <Text>Votre E-mail : </Text>
+                    <Controller
+                        
+                        control={control}
+                        rules={{
+                        maxLength: 100,
+                        }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                            placeholder="mail@email.com"
+                            onBlur={onBlur}
+                            onChangeText={onChange}
+                            value={value}
+                            style = {styles.input}
+                        />
+                        )}
+                        name="email"
                 />
                 {errors.Nom && <Text>This is required.</Text>}
             </View>
@@ -161,7 +222,7 @@ export default function ContactForm() {
                     }}
                     render={({ field: { onChange, onBlur, value } }) => (
                     <TextInput
-                        placeholder="J'ai vu une licorne traversée la route et percuter une éléphant rose"
+                        placeholder="J'ai vu une licorne traversée la route et percuter un éléphant rose"
                         onBlur={onBlur}
                         onChangeText={onChange}
                         value={value}
@@ -174,27 +235,32 @@ export default function ContactForm() {
                 />
                 {errors.Description && <Text>This is required.</Text>}
             </View>
-            
+            {locFind ? (
             <View>
+                <View style={styles.containerPinedAdresse}>
+                    <Text style={styles.pinedAdresseText}>{pinedAdresse}</Text>
+                </View>
                 <MapView 
                 style={styles.map}
                 initialRegion={mapRegion}>
                     <Marker 
                         draggable 
-                        coordinate={mapRegion} 
-                        title={pinedAdresse}
+                        coordinate={pinnedLoc} 
+                        title={"marker"}
                         onDragEnd={(e) => {setPinnedLoc(e.nativeEvent.coordinate)
                             getAdress();
                         }}
                     />
                 </MapView>
             </View>
+            ) : 
+            (<View>
 
-            <Button title="Submit" style={styles.submitButton} onPress={handleSubmit(onSubmit)}/>
+            </View>)}
+            <Button title="Envoyer" style={styles.primaryButton} onPress={handleSubmit(onSubmit)}/>
             
-            <View>
-                <Text>{pinedAdresse}</Text>
-            </View>
+            
+        </View>
         </ScrollView>
     );
 }
@@ -204,11 +270,14 @@ const styles = StyleSheet.create({
         flex: 1,
         alignContent: "center",
         justifyContent: "center",
-        padding: 50,
+        paddingRight: 50,
+        paddingLeft:50,
+        paddingTop: 5
     }, 
     map: {
         width: 300,
         height: 300,
+        marginBottom: 10,
     },
     titre: {
         fontSize: 20,
@@ -235,5 +304,25 @@ const styles = StyleSheet.create({
     },
     submitButton:{
         marginTop: 10,
+    },
+    primaryButton:{
+        borderRadius: 30,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderColor: "purple",
+        backgroundColor: "#ffffff60",
+    },
+    containerPinedAdresse :{
+        backgroundColor: "#ffffff80",
+        marginBottom: 10
+    },
+    pinedAdresseText:{
+        fontSize: 16,
+       
     }
+    
 });
